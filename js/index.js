14709b6d33494d945b7a3f06aeac833dd3312e5a
ancientElement <642211794@qq.com>
2022-08-23 18:54:24 +0800

    轮播图和主体移动端half“
    git commit -m 轮播图和主体移动端half“

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 $(function() {
-    console.log(欢迎来到ancientElement);
     $(".codehim-dropdown").CodehimDropdown({
-        skin: "deep-purple",
-        effect: 'fade'
-    });
+        skin: "deep-purple"
+       });
+        timer = setInterval(function () {
+            $('.swiper-button-next').click();
+        }, 4000)
+        //阻止点击事件冒泡
+        $('.swiper-button-next').on('click', function (e) {
+            e.stopPropagation();
+        })
+        $('.mySwiper').on('mouseleave touchend', function () {
+            console.log(22);
+            timer = setInterval(function () {
+                $('.swiper-button-next').click();
+            }, 4000)
+        })
+        $('.mySwiper').on('mouseover touchstart', function () {
+            clearInterval(timer);
+        })
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true,
+            }
+        });
 })

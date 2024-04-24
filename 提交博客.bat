be446e65790f6d9da123e8ac0c73e2fc97ccbdf2
ancientElement <642211794@qq.com>
2024-04-25 00:06:28 +0800

    vault backup: 2024-04-25 00:06:28

--- 提交博客.bat
+++ 提交博客.bat
@@ -1,7 +1,4 @@
-git pull
-git add .
-git commit -m "Auto commit at %date:~0,10%,%time:~0,8%"
-git push
+git pull --rebase --autostash
 
 hexo g && cd public && git add . && git commit -m "Auto commit at %date:~0,10%,%time:~0,8%" && git push
 @REM&& git push github
